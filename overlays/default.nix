df230a879458c57724a679e67a5d1136b18f5600
aCeTotal <lars.oksendal@gmail.com>
2025-12-27 13:44:37 +0100

    default

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,9 @@
-final: prev: {
+final: prev:
+let
+  pkgsUnstable = final.pkgsUnstable;
+in {
   winboat = final.callPackage ../pkgs/winboat { };
   winstripping = final.callPackage ../pkgs/winstripping { };
 }
+
 
