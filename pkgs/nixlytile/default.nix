31aa762a1c5548afd858c5ce522004edfd4e5dc8
aCeTotal <lars.oksendal@gmail.com>
2026-01-25 22:32:08 +0100

    gpu hybrid test

--- pkgs/nixlytile/default.nix
+++ pkgs/nixlytile/default.nix
@@ -43,7 +43,7 @@ stdenv.mkDerivation rec {
     owner = "aCeTotal";
     repo = "nixlytile";
     rev = "fa41941";
-    hash = "sha256-6udWGnsuxt0fk0QTp80v/8QhJYyatT/W8Lfas3+x46M=";
+    hash = "sha256-oZ7vdBIRyJFeCamvg5HTZ5vai86Q7hSEru+0aooinTA=";
   };
 
   nativeBuildInputs = [
