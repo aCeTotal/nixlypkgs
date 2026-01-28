6df3275b0df0b1fd7c5ce90efd004fae010de30e
aCeTotal <lars.oksendal@gmail.com>
2026-01-28 18:12:17 +0100

    testing

--- pkgs/nixlytile/default.nix
+++ pkgs/nixlytile/default.nix
@@ -43,7 +43,7 @@ stdenv.mkDerivation rec {
     owner = "aCeTotal";
     repo = "nixlytile";
     rev = "fa41941";
-    hash = "sha256-I1j1MGc2Ug5h+sfUvIqOxjBA9EKnTIvjfdpf2A6hm50=";
+    hash = "sha256-4ps4PAzsBwXk8TaFr6rma1KKW2+hU5oTlgLf1/LB4h0=";
   };
 
   nativeBuildInputs = [
