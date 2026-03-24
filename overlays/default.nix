aaa83b82729aef7a9490bc7b5f9d0b5a0bc41e41
aCeTotal <lars.oksendal@gmail.com>
2026-03-24 21:15:34 +0100

    blender

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,15 +2,11 @@ inputs: final: prev:
 let
   callPackage = final.callPackage;
 in {
-  winboat = callPackage ../pkgs/winboat { };
-  winintegration = callPackage ../pkgs/winintegration { };
-  winstripping = callPackage ../pkgs/winstripping { };
   speedtree = callPackage ../pkgs/speedtree { };
   nixlytile = callPackage ../pkgs/nixlytile { libepoxy = final.libepoxy-nixly; };
   claude = callPackage ../pkgs/claude { };
   nixlymediaserver = callPackage ../pkgs/nixlymediaserver { };
   citrix-workspace-nixly = callPackage ../pkgs/citrix-workspace-nixly { };
   nixly_steam = callPackage ../pkgs/nixly_steam { };
-  libepoxy-nixly = callPackage ../pkgs/libepoxy { };
-  xwayland = callPackage ../pkgs/xwayland { libepoxy = final.libepoxy-nixly; };
+  blender = callPackage ../pkgs/blender { };
 }
