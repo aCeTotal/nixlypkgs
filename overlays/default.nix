052e30eda9efb7482296d84b4dbe115757f57353
aCeTotal <lars.oksendal@gmail.com>
2026-04-04 19:17:53 +0200

    nixly

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,4 +10,11 @@ in {
   nixly_steam = callPackage ../pkgs/nixly_steam { };
   libepoxy-nixly = callPackage ../pkgs/libepoxy { };
   blender_nixly = callPackage ../pkgs/blender_nixly { };
+
+  # Fix flycast bundled glslang missing <cstdint> for GCC 15
+  flycast = prev.flycast.overrideAttrs (old: {
+    postPatch = (old.postPatch or "") + ''
+      sed -i '/#include "spvIR.h"/a #include <cstdint>' core/deps/glslang/SPIRV/SpvBuilder.h
+    '';
+  });
 }
